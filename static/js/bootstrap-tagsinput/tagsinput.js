e51a595a8d0f8b614dba6747bc9a22977778f308
yongxue <yongxue@yongxuedeMacBook-Pro.local>
2017-10-17 23:29:54 +0800

    利用bootstrap-tagsinput实现tag标签的输入处理

---
+++ static/js/bootstrap-tagsinput/tagsinput.js
@@ -0,0 +1,25 @@
+var citynames = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: {
+        url: '/static/js/bootstrap-tagsinput/citynames.json',
+        filter: function(list) {
+            return $.map(list, function(cityname) {
+                return { name: cityname }; });
+        }
+    }
+});
+citynames.initialize();
+
+/**
+ * Typeahead
+ */
+var elt = $('.tags_typeahead > > input');
+elt.tagsinput({
+    typeaheadjs: {
+        name: 'citynames',
+        displayKey: 'name',
+        valueKey: 'name',
+        source: citynames.ttAdapter()
+    }
+});
